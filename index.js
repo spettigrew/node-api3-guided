ac4ec9b2fd9235f6e99a1f70443dee0f4648661d
spettigrew <sara.pettigrew5@gmail.com>
2019-12-09 18:50:11 -0700

    packages installed, middleware added

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express")
+const helmet = require("helmet")
+const logger = require("./middleware/logger")
+// const morgan = require("morgan")
 const hubRouter = require("./routers/hub")
 const welcomeRouter = require("./routers/welcome")
 
 const server = express()
 
+server.use(helmet())
+server.use(logger())
+// server.use(morgan("short"))
+
 server.use(express.json())
+
 // Bring all our subroutes into the main application
 // (Remember, subroutes can have more children routers)
 server.use("/", welcomeRouter)
 server.use("/api/hubs", hubRouter)
+
+server.use((req, res) => {
+  res.status(404).json({
+    message: "Route was not found",
+  })
+})
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n")
